f346a3122bd1b89ce308ed820bcc04a27f4ba9ac
Alessandro <alessandro.boscato@gmail.com>
2020-10-20 16:42:48 +0200

    footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -197,44 +197,56 @@
     <!-- corpo -->
     <!-- footer -->
     <footer class="footer">
-      <div class="footer-column">
-        <div class="footer-column-logo">
-          <img src="{{asset('images/logo.png')}}" alt="Logo Molisana">
+      <div class="container">
+        <div class="footer-column">
+          <div class="footer-column-logo">
+            <img src="{{asset('images/logo.png')}}" alt="Logo Molisana">
+          </div>
+          <ul>
+            <li>Ragione sociale: La Molisana S.P.A.</li>
+            <li>Sede legale: Contrada Colle delle Api, 100/A - 86100 - Campobasso (CB)</li>
+            <li>Pec: lamolisana@pec.it</li>
+            <li>Tel: +39 0874 4981</li>
+            <li>Fax: +39 0874 629584</li>
+            <li>info@lamolisana.it (per segnalazioni degli utenti)</li>
+            <li>commerciale@lamolisana.it</li>
+            <li>export@lamolisana.it</li>
+            <li>numero verde 800818081</li>
+            <li>telefono 3801292455</li>
+          </ul>
         </div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
-      <div class="footer-column">
-        <h3>Pastificio</h3>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-        <h3>Pastificio</h3>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
-      <div class="footer-column">
-        <h3>Collezione da chef</h3>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
+        <div class="footer-column">
+          <h3>Pastificio</h3>
+          <ul>
+            <li>Il Pastificio</li>
+            <li>Grano decorticato a pietra</li>
+            <li>Il Molise c'è</li>
+            <li>Filiera Integrata</li>
+            <li>100 anni di pasta</li>
+            <li>Sartoria della pasta</li>
+            <li>Spaghetto Quadrato</li>
+            <li>Le Persone</li>
+          </ul>
+          <h3>Prodotti</h3>
+          <ul>
+            <li>Le Classiche</li>
+            <li>Le Integrali</li>
+            <li>Le speciali</li>
+            <li>Le Biologiche</li>
+            <li>Le Gluten-Free</li>
+            <li>Le Semole</li>
+            <li>LE Extra di Lusso</li>
+          </ul>
+        </div>
+        <div class="footer-column">
+          <h3>Collezione da chef</h3>
+          <ul>
+            <li>Collezione da Chef</li>
+            <li>Grandi Cucine</li>
+            <li>Biologiche</li>
+            <li>Quadrate</li>
+          </ul>
+        </div>
       </div>
     </footer>
     <!-- footer -->
